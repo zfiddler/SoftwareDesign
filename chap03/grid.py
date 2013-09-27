aa1ff336c49708da3774afef82c3589545c21258
Zoe Fiddler <zoe.fiddler@students.olin.edu>
2013-09-27 12:18:25 -0400

    turning in part of hw 3

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,27 @@
 """Solution to an exercise in Think Python.
 
-Author:
+Author: Zoe Fiddler
 """
 
-print '+ - - - -'
+#draws 1st, 5th and 9th line types
+def line1():
+	print '+','-','-','-','-', '+','-','-','-','-', '+'
+
+#draws other type of line 
+def line2(): 
+	print '|',' ',' ',' ' ,' ','|',' ',' ',' ' ,' ','|'
+
+def print_grid():
+	line1()
+	line2()
+	line2()
+	line2()
+	line2()
+	line1()
+	line2()
+	line2()
+	line2()
+	line2()
+	line1()
+
+print_grid()
